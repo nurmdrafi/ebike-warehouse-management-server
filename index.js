8bde83ffad5b9e60ace180f43457b7f1ab7f7a06
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-08 02:30:40 +0600

    implement jwt

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // use middleware
 app.use(cors());
@@ -24,6 +25,32 @@ async function run() {
     const inventoryCollection = client
       .db("warehouseManagement")
       .collection("inventory");
+
+    // verify jwt
+    function verifyJWT(req, res, next) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" });
+      }
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: "Forbidden access" });
+        }
+        console.log("decoded", decoded);
+        req.decoded = decoded;
+        next();
+      });
+    }
+
+    // Create jwt token while login
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "30d",
+      });
+      res.send({ accessToken });
+    });
 
     // load all items
     // https://ebike-warehouse.herokuapp.com/inventory
@@ -45,68 +72,88 @@ async function run() {
       res.send(singleItem);
     });
 
-    // load items by email
     // https://ebike-warehouse.herokuapp.com?userEmail=${email}
-    app.get("/inventory", async(req, res) =>{
-      const query = req.query;
-      console.log(query)
-      const cursor = inventoryCollection.find(query);
-      const items =  await cursor.toArray();
-      console.log(items)
-      res.send(items)
-    })
+    // app.get("/inventory", async (req, res) => {
+    //   const query = req.query;
+    //   console.log(query);
+    //   const cursor = inventoryCollection.find(query);
+    //   const items = await cursor.toArray();
+    //   console.log(items);
+    //   res.send(items);
+    // });
+
+    // load items by email
+    app.get("/inventory", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
 
     // Add new item to inventory
     // https://ebike-warehouse.herokuapp.com/add-inventory
-    app.post("/add-inventory", async (req, res) =>{
+    app.post("/add-inventory", async (req, res) => {
       const data = req.body;
       const result = await inventoryCollection.insertOne(data);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Delete item from inventory
     // https://ebike-warehouse.herokuapp.com/inventory/${_id}
-    app.delete("/inventory/:_id", async (req, res) =>{
+    app.delete("/inventory/:_id", async (req, res) => {
       const id = req.params._id;
       const query = { _id: ObjectId(id) };
       const deleteItem = await inventoryCollection.deleteOne(query);
       res.send(deleteItem);
-    })
+    });
 
     // Decrease value by 1
     // https://ebike-warehouse.herokuapp.com/inventory/${_id}
-    app.put("/inventory/:_id", async (req,res) =>{
+    app.put("/inventory/:_id", async (req, res) => {
       const id = req.params._id;
-    const data = req.body;
-    const filter = { _id: ObjectId(id) };
-    const options = { upsert: true };
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
 
-    const updateNote = {
-      $set: {
-        quantity: data.quantity,
-      },
-    };
-    const result = await inventoryCollection.updateOne(filter, updateNote, options);
-    res.send(result)
-    })
+      const updateNote = {
+        $set: {
+          quantity: data.quantity,
+        },
+      };
+      const result = await inventoryCollection.updateOne(
+        filter,
+        updateNote,
+        options
+      );
+      res.send(result);
+    });
 
     // Increase value by input
     // https://ebike-warehouse.herokuapp.com/inventory/${_id}
-    app.put("/inventory/:_id", async (req,res) =>{
-    const id = req.params._id;
-    const data = req.body;
-    const filter = { _id: ObjectId(id) };
-    const options = { upsert: true };
+    app.put("/inventory/:_id", async (req, res) => {
+      const id = req.params._id;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
 
-    const updateNote = {
-      $set: {
-        quantity: data.quantity,
-      },
-    };
-    const result = await inventoryCollection.updateOne(filter, updateNote, options);
-    res.send(result)
-    })
-
+      const updateNote = {
+        $set: {
+          quantity: data.quantity,
+        },
+      };
+      const result = await inventoryCollection.updateOne(
+        filter,
+        updateNote,
+        options
+      );
+      res.send(result);
+    });
   } finally {
     //   await client.close();
   }
