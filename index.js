73311375bd260a46be5de7ddd1b23ac188f5c565
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-09 00:51:55 +0600

    env updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")({origin: true});
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
