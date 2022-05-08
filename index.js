1aa8967a81793901d4800c9ec01cbab7ba28f193
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-08 23:27:51 +0600

    implement login api

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
+// verify jwt
+function authenticateToken(req, res, next) {
+  // const authHeader = req.headers.authorization;
+  const authHeader = req.headers['authorization']
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token =  authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iukjo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -26,28 +45,11 @@ async function run() {
       .db("warehouseManagement")
       .collection("inventory");
 
-    // verify jwt
-    function verifyJWT(req, res, next) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
-        return res.status(401).send({ message: "Unauthorized access" });
-      }
-      const token = authHeader.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(403).send({ message: "Forbidden access" });
-        }
-        console.log("decoded", decoded);
-        req.decoded = decoded;
-        next();
-      });
-    }
-
     // Create jwt token while login
     app.post("/login", async (req, res) => {
       const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "30d",
+        expiresIn: "1d",
       });
       res.send({ accessToken });
     });
@@ -83,7 +85,7 @@ async function run() {
     // });
 
     // load items by email
-    app.get("/inventory", verifyJWT, async (req, res) => {
+    app.get("/inventory", authenticateToken, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.query.email;
       if (email === decodedEmail) {
